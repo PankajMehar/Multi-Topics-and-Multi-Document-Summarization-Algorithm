524aed3aa45ac3e97335f9c38b7dd08d0211d57c
moveurbody <a_gangan@hotmal.com>
2017-11-26 00:11:51 +0800

    需要重新寫idf的邏輯部分

--- term_weighting.py
+++ term_weighting.py
@@ -6,10 +6,12 @@
 
 import numpy as np
 import copy
+import math
 from log_module import log
 
 # 轉換成字詞向量
 def text_to_vector(corpus_list):
+    log("[text_to_vector][start]",lvl="i")
     # 產生獨立字詞
     word_list = []
     for i in range(len(corpus_list)):
@@ -31,41 +33,55 @@ def text_to_vector(corpus_list):
             corpus_list[raw] = [word.lower() for word in corpus_list[raw]]
             temp.append(corpus_list[raw].count(word_list[word]))
         word_array.append(temp)
+    log("[text_to_vector]\n%s" % np.asarray(word_array))
+    log("[text_to_vector][end]", lvl="i")
     return word_array
 
 # 計算tf
 def tf(array):
+    log("[tf][start]",lvl="i")
     # 建立資料tf資料陣列
     tf_array=copy.deepcopy(array)
-    log("tf_array:\n" + str(tf_array))
-    log("array:\n" + str(array))
 
     for raw in range(1,len(tf_array)):
         word_cout = sum(tf_array[raw])
         for i in range(len(tf_array[raw])):
-            # 這行會導致變數被污染
             tf_array[raw][i] = tf_array[raw][i]/word_cout
-    # print(np.asarray(tf_array))
-    log("tf_array:\n"+str(tf_array))
-    log("array:\n"+str(array))
+    log("[tf] \n%s" % np.asarray(tf_array))
+    log("[tf][end]",lvl="i")
 
 # 計算idf
 def idf(array):
+    log("[idf][start]",lvl="i")
     # 建立資料idf陣列資料
-    idf_array = array
+    idf_array = copy.deepcopy(array)
+    # 建立暫存的資料做idf運算
+    temp = copy.deepcopy(idf_array)
     # 總文件數
     file_count = len(array)
+    word_count = len(array[0])
     # 先進行轉置
-    array = [[row[i] for row in array] for i in range(len(array[0]))]
-    print(np.asarray(idf_array))
+    temp = [[row[i] for row in temp] for i in range(len(temp[0]))]
+    # 顯示暫存資料的內容，確定已經轉置
+    log(np.asarray(temp))
+
+    for word in range(word_count):
+        word_in_file = 0
+        for i in range(1,word):
+            if word_count[word][i] > 0:
+                word_in_file = word_in_file+1
+        log(word_in_file,word_count)
+        # idf_value = math.log10(word_in_file/word_count)
+        idf_value = word_in_file
+        for i in range(1,word):
+            temp[i][word] = idf_value
+    log("[idf] %s\n" % np.asarray(temp))
+    log("[idf][end]", lvl="i")
 
 # 計算tf_idf
 def tf_idf(corpus_list):
     vector = text_to_vector(corpus_list)
-    log(vector)
-    print("=======================\n")
     tf(vector)
-    print("=======================\n")
     idf(vector)
 
 
