4516827cc498a2e8a766fd44ba24386d35899fb6
moveurbody <a_gangan@hotmail.com>
2018-01-19 21:25:05 +0800

    將分析的資料暫存在dictionary中，之後會再輸出

--- analysis_auto.py
+++ analysis_auto.py
@@ -14,30 +14,46 @@ import re
 import numpy as np
 import pandas
 from sklearn import metrics
+import json
 
 
-def main():
+def similuity(file_path):
+    # 儲存用的資料結構
+    result = {}
+
     # 先取得目錄下所有的檔案名稱
-    file_list = get_file_list('/Users/yuhsuan/Desktop/MEMDS/arrange_day_30/')
+    file_list = get_file_list(file_path)
 
     file_info = []
     # 取得每個資料的日,來源,主題
     for i in file_list:
-        m = re.match('.*day(\d+)_(.*)_(.*)_.*.txt',i)
-        file_info.append([m.group(0),int(m.group(1)),m.group(2),m.group(3)])
+        m = re.match('.*day(\d+)_(.*)_(.*)_.*.txt', i)
+        file_info.append([m.group(0), int(m.group(1)), m.group(2), m.group(3)])
 
     # 顯示所有的資料
-    log('%s\n\n' % file_info,lvl = 'i')
+    log('%s\n\n' % file_info, lvl='i')
 
     # 取得最大的日子
-    df = pandas.DataFrame(file_info,columns=['file_name','day','source','theme'])
+    df = pandas.DataFrame(file_info, columns=['file_name', 'day', 'source', 'theme'])
     max_day = max(df['day'])
-    log('max_day: %s' % max_day,lvl='i')
+    log('max_day: %s' % max_day, lvl='i')
 
+    result['max_day']=max_day
+    result['daily_data'] = []
+    result_daily_data = result['daily_data']
     # 處理每一天的數據
     for day in list(set(df['day'])):
-        log('===='*20,lvl='i')
-        log('process day: %s data' % day,lvl='i')
+        log('====' * 20, lvl='i')
+        log('process day: %s data' % day, lvl='i')
+
+        # 紀錄第幾天的資料
+        daily_result = {}
+        daily_result['day'] = day
+        daily_result['process_day'] = []
+        daily_result['cos'] = []
+        daily_result['tf-idf'] = []
+        daily_result['tf-pdf'] = []
+
         # 每天的所有資料
         file_info_days = []
 
@@ -46,26 +62,26 @@ def main():
                 file_info_days.append(i)
 
         # 轉換成dataframe
-        df = pandas.DataFrame(file_info_days,columns=['file_name','day','source','theme'])
-        log('Day %s data:\n %s' % (day,df),lvl='i')
+        df = pandas.DataFrame(file_info_days, columns=['file_name', 'day', 'source', 'theme'])
+        log('Day %s data:\n %s' % (day, df), lvl='i')
         source = list(set(df['source']))
-        log('source: %s' % source,lvl = 'i')
+        log('source: %s' % source, lvl='i')
 
         # 建立channel_list給TF_PDF用
         channel_list = []
         for i in df['source']:
             for j in range(len(source)):
                 if i == source[j]:
-                    channel_list.append(j+1)
-        log('channel_list: %s' % channel_list,lvl = 'i')
+                    channel_list.append(j + 1)
+        log('channel_list: %s' % channel_list, lvl='i')
 
         # 建立day_one_data取得每一天內的檔案們
         day_one_data = df['file_name']
 
-        document_list=[]
+        document_list = []
         for data in day_one_data:
             # data = '/Users/yuhsuan/Desktop/MEMDS/news_documents_r/day0_cnn_cr_0_r.txt'
-            file = open(data, 'r', encoding = 'utf8')
+            file = open(data, 'r', encoding='utf8')
             # print(str(data))
             result = file.readline().strip().split(' ')
             document_list.append(result)
@@ -75,14 +91,14 @@ def main():
         tf_idf_cos = []
         tf_idf_pre = []
         tf_idf_act = []
-        tf_idf_criteria = 0.05 #設定準確率
+        tf_idf_criteria = 0.05  # 設定準確率
         # tf_pdf的預測與結果
         tf_pdf_pre = []
         tf_pdf_act = []
         tf_pdf_criteria = 0.1
 
         # 計算tf_idf資料
-        log('計算tf-idf資料'+'===='*20,lvl='i')
+        log('計算tf-idf資料' + '====' * 20, lvl='i')
         res = tf_idf(document_list)
         log('====' * 20)
         log('tf-idf: %s' % res)
@@ -91,48 +107,65 @@ def main():
 
         # 計算tf-idf相似度
         log('計算tf-idf相似度' + '====' * 20, lvl='i')
-        matrix = [[None]*(len(res)-1) for i in range(len(res)-1)]
+        matrix = [[None] * (len(res) - 1) for i in range(len(res) - 1)]
         for i in range(1, len(res)):
             for j in range(1, i):
                 log("%s, %s" % (i, j), lvl='i')
+                daily_result['process_day'].append((i,j))
+
                 cos = cosines(res[i], res[j])
+                daily_result['tf-idf'].append(cos)
 
                 tf_idf_cos.append(cos)
-                # 預測部分
-                if cos>=tf_idf_criteria:
-                    tf_idf_pre.append(True)
-                else:
-                    tf_idf_pre.append(False)
 
-                # 真實部分
-                if df['theme'][i-1]==df['theme'][j-1]:
-                    tf_idf_act.append(True)
-                else:
-                    tf_idf_act.append(False)
+        #         # 預測部分
+        #         if cos >= tf_idf_criteria:
+        #             tf_idf_pre.append(True)
+        #         else:
+        #             tf_idf_pre.append(False)
+        #
+        #         # 真實部分
+        #         if df['theme'][i - 1] == df['theme'][j - 1]:
+        #             tf_idf_act.append(True)
+        #         else:
+        #             tf_idf_act.append(False)
+        #
+        #         matrix[j - 1][i - 1] = cos
+        # log('tf-idf: \n%s' % matrix, lvl='i')
+        # log('tf_idf_cos: %s,tf_idf_pre: %s, tf_idf_act: %s' % (tf_idf_cos, tf_idf_pre, tf_idf_act), lvl='i')
+        # # from sklearn import metrics
+        #
+        # log('\n\nday: %s, files: %s, accuracy: %s, precision: %s, recall: %s, f1-score: %s\n\n' % (
+        # day, len(df), metrics.accuracy_score(tf_idf_act, tf_idf_pre), metrics.precision_score(tf_idf_act, tf_idf_pre),
+        # metrics.recall_score(tf_idf_act, tf_idf_pre), metrics.f1_score(tf_idf_act, tf_idf_pre)), lvl='i')
+        #
 
-                matrix[j-1][i-1] = cos
-        log('tf-idf: \n%s' % matrix,lvl='i')
-        log('tf_idf_cos: %s,tf_idf_pre: %s, tf_idf_act: %s' % (tf_idf_cos,tf_idf_pre,tf_idf_act),lvl='i')
-        # from sklearn import metrics
-
-        log('\n\nday: %s, files: %s, accuracy: %s, precision: %s, recall: %s, f1-score: %s\n\n' % (day,len(df),metrics.accuracy_score(tf_idf_act, tf_idf_pre),metrics.precision_score(tf_idf_act, tf_idf_pre),metrics.recall_score(tf_idf_act, tf_idf_pre),metrics.f1_score(tf_idf_act, tf_idf_pre)),lvl='i')
         # 計算tf_pdf資料
         log('計算tf-pdf資料' + '====' * 20, lvl='i')
         res = tf_pdf(document_list, channel_list)
         log('====' * 20)
         log(res)
         log('====' * 20)
-        log('我是什麼東東？%s' % len(res[0]),lvl='i')
+        log('我是什麼東東？%s' % len(res[0]), lvl='i')
 
         # 計算tf-pdf相似度
         log('計算tf-pdf相似度' + '====' * 20, lvl='i')
-        matrix = [[None] * (len(res)-1) for i in range(len(res)-1)]
+        matrix = [[None] * (len(res) - 1) for i in range(len(res) - 1)]
         for i in range(1, len(res)):
-            for j in range(1, i+1):
+            for j in range(1, i + 1):
                 log("%s, %s" % (i, j), lvl='i')
                 cos = cosines(res[i], res[j])
-                matrix[j-1][i-1] = cos
+                daily_result['tf-pdf'].append(cos)
+                matrix[j - 1][i - 1] = cos
         log('tf-pdf: \n%s' % matrix, lvl='i')
+        print(daily_result)
+        # result['daily_data'].append(daily_result)
+        result_daily_data.append(daily_result)
+
+def main():
+    file_path = '/Users/yuhsuan/Desktop/MEMDS/arrange_day_30/'
+    similuity(file_path)
+
 
 if __name__ == '__main__':
     main()
