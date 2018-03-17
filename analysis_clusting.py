c1eba79f82267f39f612d26f84e98cc79d5ef8c5
moveurbody <a_gangan@hotmail.com>
2018-03-17 23:50:50 +0800

    最後算出來精準度最高的關係度為0.17, 資料在arrange_day_0\res.csv

--- analysis_clusting.py
+++ analysis_clusting.py
@@ -6,12 +6,23 @@
 import json
 import os
 import re
+import pandas as pd
+from log_module import log
+from sklearn import metrics
 
 def main():
     answer_file = r"C:\Users\Yuhsuan\Desktop\MEMDS\arrange_day_0\answer.json"
     answer = {}
     with open(answer_file,'r') as file:
         answer = json.load(file)
+
+    res = []
+    for i in range(1,100):
+        pre = process_file(r"C:\Users\Yuhsuan\Desktop\MEMDS\arrange_day_0\final_group_file_reference\{}.json".format(i))
+        log(i,lvl='i')
+        res.append(metrics_value(answer,pre))
+    df = pd.DataFrame(res)
+    df.to_csv(r"C:\Users\Yuhsuan\Desktop\MEMDS\arrange_day_0\res.csv", sep=',', encoding='utf-8')
 
 # 計算正確的GROUP
 def correct_group():
@@ -74,7 +85,145 @@ def correct_group():
 
 # 處理檔案
 def process_file(file_path):
+    data = {}
+    with open(file_path,'r') as file:
+        data = json.load(file)
 
+    # 暫時寄放的，用來記錄每一個Group所屬於的分群與編號
+    temp = []
 
+    for group in data:
+        # 先判斷裡面的資料應該屬於哪個比較的GROUP
+        source_data = {
+            'missile': [],
+            'turkish': [],
+            'catalan': [],
+            'brexit': [],
+            'gravitational': [],
+            'hk': [],
+            'sewol': [],
+            'syria': [],
+            'crimea': []
+        }
+
+        missile = source_data['missile']
+        turkish = source_data['turkish']
+        catalan = source_data['catalan']
+        brexit = source_data['brexit']
+        gravitational = source_data['gravitational']
+        hk = source_data['hk']
+        sewol = source_data['sewol']
+        syria = source_data['syria']
+        crimea = source_data['crimea']
+
+        for i in data[group]:
+            if 'missile' in i:
+                missile.append(1)
+            elif "turkish" in i:
+                turkish.append(2)
+            elif "catalan" in i:
+                catalan.append(3)
+            elif "brexit" in i:
+                brexit.append(4)
+            elif "gravitational" in i:
+                gravitational.append(5)
+            elif "hk" in i:
+                hk.append(6)
+            elif "sewol" in i:
+                sewol.append(7)
+            elif "syria" in i:
+                syria.append(8)
+            elif "crimea" in i:
+                crimea.append(9)
+            else:
+                pass
+
+        # print("source_data: %s" % source_data)
+
+        # 按照分類的數量大小進行合併
+        lisr = []
+        for i in source_data:
+            lisr.append([i,len(source_data[i])])
+        df = pd.DataFrame(lisr,columns=['type','number'])
+        df.sort_values(by=['number'], ascending=[False],inplace=True)
+        lisr = df['type'].values.tolist()
+
+        les = []
+        for i in lisr:
+            les.extend(source_data[i])
+        temp.append({lisr[0]:les})
+
+    system_group = {}
+
+    check_list = ['missile','turkish','catalan','brexit','gravitational','hk','sewol','syria','crimea']
+    for event_num in range(len(check_list)):
+        temp_group_list = []
+        for i in temp:
+            if check_list[event_num] in i:
+                temp_group_list.append(i)
+        log("temp_group_list: %s" % temp_group_list)
+        if len(temp_group_list)==0:
+            system_group[check_list[event_num]]=[]
+        elif len(temp_group_list)==1:
+            system_group[check_list[event_num]] = temp_group_list[0][check_list[event_num]]
+        else:
+            # 最多正確的資料
+            number = []
+            number_count = []
+            number_max = []
+            for i in range(len(temp_group_list)):
+                log("event_num: %s" % event_num)
+                number.append(i)
+                number_count.append(temp_group_list[i][check_list[event_num]].count(event_num+1))
+                number_max.append(len(temp_group_list[i][check_list[event_num]]))
+
+            select_one=""
+            for j in number:
+                if max(number_count) == number_count[j]:
+                    select_one=j
+            log('{} {} {}'.format(number,number_count,number_max))
+            log("select_one: %s" % select_one)
+            system_group[check_list[event_num]] = temp_group_list[select_one][check_list[event_num]]
+        log("system_group: %s" % system_group)
+        log("\n\n")
+    return system_group
+
+# 計算相關預測數值
+def metrics_value(real_data,system_data):
+    check_list = ['missile', 'turkish', 'catalan', 'brexit', 'gravitational', 'hk', 'sewol', 'syria', 'crimea']
+    item_number = 1
+    res = []
+    for event in check_list:
+        real_count = len(real_data[event])
+        system_count = len(system_data[event])
+
+        # 如果系統的資料比較少
+        if real_count>system_count:
+            add = [0 for i in range(real_count-system_count)]
+            system_data[event].extend(add)
+        elif real_count<system_count:
+            add = [0 for i in range(system_count - real_count)]
+            real_data[event].extend(add)
+        else:
+            pass
+
+        log(real_data[event])
+        log(system_data[event])
+
+        if item_number not in system_data[event]:
+            accuracy = 0.0
+            precision = 0.0
+            recall = 0.0
+            f1_score = 0.0
+        else:
+            log("xxxx")
+            accuracy = metrics.accuracy_score(real_data[event], system_data[event])
+            precision = metrics.precision_score(real_data[event], system_data[event],average='micro')
+            recall = metrics.recall_score(real_data[event], system_data[event],average='micro')
+            f1_score = metrics.f1_score(real_data[event], system_data[event],average='micro')
+        log('{},{},{},{},{}'.format(event,accuracy,precision,recall,f1_score),lvl='i')
+        res.extend([accuracy,precision,recall,f1_score])
+        item_number=item_number+1
+    return res
 if __name__ == "__main__":
     main()
